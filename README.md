c9661cdf638f5f2f019fa495185f2a5089631bfa
ky-nguyen <ky-nguyen@ubuntu22.4>
2022-10-19 19:54:53 +0700

    install theme

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 This project create website for user can learn programming languages
 So user can create room, talk and chat with.(Restricted Pages)
 User can registration!
+
+
+![img.png](img.png)
+
+--> Create a virtual environment
+For windows: 
+* pip install virtualenv
+* virtualenv envname
+--> Activate the virtual environment :
+* envname\scripts\activate
+--> Install the requirements :
+pip install -r requirements.txt
+
+<h1>Running the App:</h1>
+--> To run the App, we use :
+python manage.py runserver
+
+
+* So you can run for db other or docker!
+* <hr>
+I'm doing
--- base/views.py
+++ base/views.py
@@ -21,10 +21,13 @@ def home(request):
     topics = Topic.objects.all()
 
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
     context = {
         'rooms': rooms,
         'topics': topics,
         'room_count': room_count,
+        'room_messages': room_messages,
     }
     return render(request, 'base/home.html', context)
 
@@ -60,7 +63,10 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
+
             return redirect('home')
 
     context = {
@@ -168,3 +174,18 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': message})
+
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+
+    context = {
+      'user': user,
+      'rooms': rooms,
+      'room_messages': room_messages,
+      'topics': topics,
+    }
+    return render(request, 'base/profile.html', context)
